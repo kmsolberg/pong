eccb768f3c9e5df4fcc5833a0c7907719835861d
Kat Solberg <katsolberg@gmail.com>
2017-05-12 16:07:09 -0700

    refactor code

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -1,7 +1,7 @@
 import { SVG_NS } from '../settings';
 
 export default class Ball {
-  
+
   constructor(radius, boardWidth, boardHeight) {
     this.radius = radius;
     this.boardWidth = boardWidth;
@@ -11,72 +11,66 @@ export default class Ball {
 
     this.reset();
   }
-  
+
   reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
 
     // generate a random number between -5 and 5
     this.vy = 0;
-
-    while (this.vy === 0 ) {
-      this.vy = Math.floor(Math.random() * 10 - 5); 
+    while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
     }
-
     // a number between -5 and 5, based on the vy
-    this.vx = this.direction * (6 - Math.abs(this.vy)); 
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
-  wallCollision(){
-    const hitLeft = this.x - this.radius <= 0;
-    const hitRight = this.x + this.radius >= this.boardWidth;
+  wallCollision() {
     const hitTop = this.y - this.radius <= 0;
     const hitBottom = this.y + this.radius >= this.boardHeight;
 
-    if (hitLeft || hitRight) {
-      this.vx = -this.vx;
-    } else if (hitTop || hitBottom) {
+    if (hitTop || hitBottom) {
       this.vy = -this.vy;
     }
   }
-    paddleCollision(player1, player2) {
-      // if moving toward the right
-      if (this.vx > 0) {
-        // check for collision on Player 2
-        let paddle = player2.coordinates(player2.x, player2.y, player2.width, player2.height);
-        let [leftX, rightX, topY, bottomY] = paddle;
+  paddleCollision(player1, player2) {
+    // if moving toward the right
+    if (this.vx > 0) {
+      // check for collision on Player 2
+      let paddle = player2.coordinates(player2.x, player2.y, player2.width, player2.height);
+      let [leftX, rightX, topY, bottomY] = paddle;
 
-        if (
-          this.x + this.radius >= leftX // the right edge of the ball is >= left edge of the paddle
-          && this.x + this.radius <= rightX // the right edge of the ball is <= right edge paddle
-          && this.y >= topY // the ball Y is >= the top edge of the paddle
-          && this.y <= bottomY // the ball is <= the bottom edge of the paddle
-        ) {
-          this.vx = -this.vx
-          this.ping.play();
-        }
-      } else {
-        let paddle = player1.coordinates(player1.x, player1.y, player1.width, player1.height);
-        let [leftX, rightX, topY, bottomY] = paddle;
+      if (
+        this.x + this.radius >= leftX // the right edge of the ball is >= left edge of the paddle
+        && this.x + this.radius <= rightX // the right edge of the ball is <= right edge paddle
+        && this.y >= topY // the ball Y is >= the top edge of the paddle
+        && this.y <= bottomY // the ball is <= the bottom edge of the paddle
+      ) {
+        this.vx = -this.vx
+        this.ping.play();
+      }
+    } else {
+      let paddle = player1.coordinates(player1.x, player1.y, player1.width, player1.height);
+      let [leftX, rightX, topY, bottomY] = paddle;
 
-          if (
-            this.x - this.radius <= rightX
-            && this.x - this.radius >= leftX
-            && this.y >= topY
-            && this.y <= bottomY
-          ) {
-            this.vx = -this.vx
-            this.ping.play();
-          }        
-        }
-    } 
-  
-  goal (player){
+      if (
+        this.x - this.radius <= rightX
+        && this.x - this.radius >= leftX
+        && this.y >= topY
+        && this.y <= bottomY
+      ) {
+        this.vx = -this.vx
+        this.ping.play();
+      }
+    }
+  }
+
+  goal(player) {
     player.score++;
     this.reset();
   }
 
-  render (svg, player1, player2){
+  render(svg, player1, player2) {
     this.x += this.vx;
     this.y += this.vy;
 
