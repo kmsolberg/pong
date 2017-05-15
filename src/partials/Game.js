2aa66c0d7ea1590d1da4f5a45c023e48dff16bac
Kat Solberg <katsolberg@gmail.com>
2017-05-14 17:08:26 -0700

    stretch goals work

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -10,7 +10,6 @@ export default class Game {
 	constructor(element, width, height) {
 		this.width = width;
 		this.height = height;
-
 	
 	this.gameElement = document.getElementById(element);
 	this.board = new Board(this.width, this.height);	
@@ -18,6 +17,15 @@ export default class Game {
 	this.paddleHeight = 56;
 	this.padding = 10;
 	this.radius = 8;
+	let svg = document.createElementNS(SVG_NS, 'svg');
+	
+
+	function multipleBalls () {
+		var ball = new Ball(this.radius, this.width, this.height)
+		for (var i = 0; i < 10; i++) {
+			ball[i] = new Ball(this.radius, this.width, this.height)
+		} return ball[i];
+	}
 
 	this.player1 = new Paddle(this.height, 
 		this.paddleWidth, 
@@ -33,8 +41,7 @@ export default class Game {
 		(height - this.paddleHeight) / 2,
 		KEYS.up,
 		KEYS.down);
-	this.ball = new Ball(this.radius, this.width, this.height);
-
+		
 	this.score1 = new Score(this.width/2 - 35, 20, this.size);
 	this.score2 = new Score(this.width/2 + 25, 20, this.size);
 
@@ -43,7 +50,7 @@ export default class Game {
 			this.pause = !this.pause;
 		}
 	})
-	}
+}
 
 	render() {
 		if (this.pause) {
@@ -57,13 +64,17 @@ export default class Game {
 		svg.setAttributeNS(null, 'height', this.height);
 		svg.setAttributeNS(null, 'ViewBox', `0 0 ${this.width} ${this.height}`);
 		this.gameElement.appendChild(svg);
-
+		
 		this.board.render(svg);
 		this.player1.render(svg);
 		this.player2.render(svg);
-		this.ball.render(svg, this.player1, this.player2);
+		this.ball.render(svg, this.player1, this.player2);	
+		// this.ball1.render(svg, this.player1, this.player2);							
 		this.score1.render(svg, this.player1.score);
 		this.score2.render(svg, this.player2.score);
-	}
 
+		this.multipleBalls();
+		
+	}
+	
 }
